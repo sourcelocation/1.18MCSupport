0775f9944d646f533dd03305a54837a49b42b07f
Matthew Anisovich <anisovich.matthew@gmail.com>
2021-09-15 16:12:59 +0300

    Rewrite

--- main.py
+++ main.py
@@ -1,58 +1,69 @@
 import argparse
 from zipfile import *
-from shutil import make_archive, rmtree
+from shutil import rmtree
 import json
 import os
 from pathlib import Path
-from support import log
+from support import log, make_archive
 
-parser = argparse.ArgumentParser(description='Make mods support 1.18.')
-parser.add_argument('path', metavar='PATH', type=str, nargs='+',
-					help='the path to the .jar or folder of jars')
-args = parser.parse_args()
+TMP_FOLDER = Path("tmp")
+OUTPUT_FOLDER = Path("output")
 
-passed_path = args.path[0]
-paths = []
-
-if passed_path.split("/")[-1].count(".jar") == 0:
-	# Passed a folder
-	paths = os.listdir(passed_path)
-	if ".DS_Store" in paths:
-		paths.remove(".DS_Store")
-	for i,path in enumerate(paths):
-		paths[i] = passed_path + "/" + path
-else:
-	paths = [passed_path]
-
-if not os.path.exists("tmp"):
-	os.makedirs("tmp")
-
-for path in paths:
-	log("Unzipping " + path + "...")
-	#Unzip
-	mod_name = (path.split("/")[-1]).split(".jar")[0]
-	res_folder = f"tmp/{mod_name}"
-	with ZipFile(path) as zip: 
-		zip.extractall(res_folder)
-	
-	#Edit
+def simple_mod_dependency_edit(mod_contents):
 	log("Editing " + path + "...")
-	fabric_mod_file_dir = res_folder + "/fabric.mod.json"
+	fabric_mod_file_dir = os.path.join(mod_contents, "fabric.mod.json")
 	fabric_mod_json = ""
 	with open(fabric_mod_file_dir, "r") as file:
 		fabric_mod_json = json.loads(file.read())
 		fabric_mod_json["depends"]["minecraft"] = ["~1.16","~1.17","~1.18"]
-	
+		
 	with open(fabric_mod_file_dir, "w") as file:
 		file.write(json.dumps(fabric_mod_json))
 		file.close()
+
+def preparation():
+	if not os.path.exists("tmp"):
+		os.makedirs("tmp")
+	if not os.path.exists("output"):
+		os.makedirs("output")
+
+def parse_paths():
+	parser = argparse.ArgumentParser(description='Make mods support 1.18.')
+	parser.add_argument('path', metavar='PATH', type=str, nargs='+',
+						help='the path to the .jar or folder of jars')
+	args = parser.parse_args()
+	passed_path = args.path[0]
+	paths = []
+	if passed_path.split("/")[-1].count(".jar") == 0:
+		# Passed a folder
+		paths = os.listdir(passed_path)
+		if ".DS_Store" in paths:
+			paths.remove(".DS_Store")
+		for i,path in enumerate(paths):
+			paths[i] = os.path.join(passed_path, path)
+	else:
+		paths = [passed_path]
+	
+	return paths
+
+
+preparation()
+paths = parse_paths()
+for path in paths:
+	log("Unzipping " + path + "...")
+	#Unzip
+	mod_name = (path.split("/")[-1]).split(".jar")[0]
+	mod_contents = os.path.join(TMP_FOLDER, mod_name)
+	with ZipFile(path) as zip: 
+		zip.extractall(mod_contents)
+	
+	#Edit
+	simple_mod_dependency_edit(mod_contents)
 	
 	#Zip it back
 	log("Zipping " + path + "...")
-	make_archive(mod_name, 'zip', root_dir=res_folder, base_dir=None)
-	log("Moving " + path + "to 'output/'")
-	os.rename(mod_name + ".zip", mod_name + ".jar")
-	Path(mod_name + ".jar").rename("output/" + mod_name + ".jar")
+	make_archive(mod_contents, os.path.join(OUTPUT_FOLDER, mod_name + ".zip"))
+	os.rename(os.path.join(OUTPUT_FOLDER,mod_name + ".zip"), os.path.join(OUTPUT_FOLDER,mod_name + "_1.18.jar"))
 	
 log("Cleaning up...")
 rmtree("tmp")
--- support.py
+++ support.py
@@ -1,3 +1,13 @@
+import os, shutil
+def make_archive(source, destination):
+	base = os.path.basename(destination)
+	name = ".".join(base.split('.')[0:-1])
+	format = base.split('.')[-1]
+	archive_from = os.path.dirname(source)
+	archive_to = os.path.basename(source.strip(os.sep))
+	shutil.make_archive(name, format, archive_from, archive_to)
+	shutil.move('%s.%s'%(name,format), destination)
+
 debug = True
 
 def log(string):
